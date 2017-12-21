f40023b104e9f1e17fb0dcb84546f20e6bb0799b
pirate <pirate.dmitry@gmail.com>
2017-12-21 13:51:18 +0200

    home component fix

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,6 +12,7 @@ require('./bootstrap');
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+Vue.component('home', require('./components/Site/Home.vue'));
 
 const app = new Vue({
     el: '#app',
